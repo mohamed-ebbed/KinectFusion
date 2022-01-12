93747606d8dac0b992571b3ae46d9ac4efeccc78
Nijat Ibrahimov <ibrahimovnijat@gmail.com>
2022-01-12 12:53:18 +0100

    Main file modified. bilateral filter applied, vertex map computed. Normals map needs revision urgently

--- main.cpp
+++ main.cpp
@@ -3,16 +3,24 @@
 #include <opencv2/core/cuda.hpp>
 #include <opencv2/cudaimgproc.hpp>
 
+#include "VirtualSensor.h"
+#include <vector>
+
+
 using namespace std;
 using namespace cv;
 using namespace dnn;
 using namespace cuda;
 
-#include "VirtualSensor.h"
-#include <vector>
+struct Vertex {
+//    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    Vector4f pos;
+};
+
 
 int main()
 {
+//    printCudaDeviceInfo(0);
     // Make sure this path points to the data folder
     std::string filenameIn = "../Data/rgbd_dataset_freiburg1_xyz/";
     std::string filenameBaseOut = "mesh_";
@@ -27,62 +35,68 @@ int main()
         std::cout << "File Opened" << std::endl;
     }
 
-    sensor.processNextFrame();
+//    int sensorFrame = 0;
+    while(sensor.processNextFrame()) {
+        float* depthMat = sensor.getDepth();
+        unsigned int depthWidth = sensor.getDepthImageWidth();
+        unsigned int depthHeight = sensor.getDepthImageHeight();
 
-    float* depthMat = sensor.getDepth();
-    uint32_t depthWidth = sensor.getDepthImageWidth();
-    uint32_t depthHeight = sensor.getDepthImageHeight();
+        Matrix3f depthIntrinsics = sensor.getDepthIntrinsics();     // get K matrix (intrinsics), global to camera frame
+        Matrix3f depthIntrinsicsInv = depthIntrinsics.inverse();
+        cout << depthIntrinsicsInv.size() << endl;
+        break;
 
-    cv::Mat depth_Mat = cv::Mat(static_cast<int>(depthHeight), static_cast<int>(depthWidth), CV_8U, depthMat);
-    cv::Mat filt_dept_Mat;
+        cv::Mat depth_mat = cv::Mat(static_cast<int>(depthHeight), static_cast<int>(depthWidth), CV_32F, depthMat);
+        cv::Mat filt_depth_mat(depth_mat.size(), CV_32F);
+        cv::Mat depth_normals(depth_mat.size(), CV_32F);
+        cv::Mat vertex_validity(depth_mat.size(), CV_32F);  //binary matrix for valid depth vals
 
-    cuda::GpuMat gpuImage;
-    gpuImage.upload(depth_Mat);
-    if (gpuImage.empty()) {
-        cout << "could not upload data to gpu " << endl;
-    } else {
-        cout << "gpu works yayy!" << endl;
-        //max d val of 5 recommended for real-time applications (9 for offline) for cpu.
-        //for gpu can be higher
-        cv::cuda::bilateralFilter(gpuImage, gpuImage, 20, 50, 50, BORDER_DEFAULT);
-    }
-    gpuImage.download(depth_Mat);
-    cout << depth_Mat.size() << endl;
 
-    cv::Mat depth_normals(depth_Mat.size(), CV_32FC3);
+        //appy bilateralFilter to raw depth
+        cv::bilateralFilter(depth_mat, filt_depth_mat, 5, 50,50, BORDER_DEFAULT); //max d val of 5 recommended for real-time applications (9 for offline)
+        cout << "depth mat size: " << depth_mat.size() << endl;
+        cout << "filt_depthmat size: " << filt_depth_mat.size() << endl;
 
-    //compute normals
-    cout << "before normals" << endl;
-    for (int x =0; x < depth_Mat.rows; ++x) {
-        for (int y = 0; y < depth_Mat.cols; ++y) {
-            float dzdx = (depth_Mat.at<float>(x+1,y) - depth_Mat.at<float>(x-1,y)) / 2.0f;
-            float dzdy = (depth_Mat.at<float>(x,y+1) - depth_Mat.at<float>(x,y-1)) / 2.0f;
-            Vec3f d(-dzdx, -dzdy, 1.0f);
-            Vec3f n = cv::normalize(d);
-            depth_normals.at<Vec3f>(x,y) = n;
+        //get vertices map in sensor/camera frame
+        Vertex* vertices = new Vertex[depthWidth*depthHeight];
+
+        unsigned int vertex_idx = -1;
+        for (unsigned int r = 0; r < depthHeight; r++) {
+            for (unsigned int c = 0; c < depthWidth; c++) {
+                vertex_idx += 1;
+                float depth_pixel = filt_depth_mat.at<float>(r,c);
+                if (isnan(depth_pixel)) {
+                    vertex_validity.at<int>(r,c) = 0;
+//                    vertices[vertex_idx].pos = Vector4f(MINF, MINF, MINF, MINF);
+                } else{
+                    cout << depthIntrinsicsInv.size() << endl;
+                    Vector3f camera_coord = depthIntrinsicsInv * Vector3f(r,c,1) * depth_pixel;
+                    vertices[vertex_idx].pos[0] = camera_coord[0];
+                    vertices[vertex_idx].pos[1] = camera_coord[1];
+                    vertices[vertex_idx].pos[2] = camera_coord[2];
+                    vertices[vertex_idx].pos[3] = 1.0f;
+                    vertex_validity.at<int>(r,c) = 1;
+                }
+            }
         }
+
+//        //compute normals of the vertex map (in camera frame)
+//        vertex_idx = -1;
+//        for (int x =0; x < depthHeight; ++x) {
+//            for (int y = 0; y < depthWidth; ++y) {
+//                vertex_idx += 1;
+//                float dzdx = (vertices[vertex_idx].pos(x+1) - vertices[vertex_idx].pos(7)
+//                float dzdx = (depth_mat.at<float>(x+1,y) - depth_mat.at<float>(x-1,y)) / 2.0f;
+//                float dzdy = (depth_mat.at<float>(x,y+1) - depth_mat.at<float>(x,y-1)) / 2.0f;
+//                Vec3f d(-dzdx, -dzdy, 1.0f);
+//                Vec3f n = cv::normalize(d);
+//                depth_normals.at<Vec3f>(x,y) = n;
+//            }
+//        }
+
+        break;
+        delete[] vertices;  //delete heap
     }
-    cout << "Normals ready?!" << endl;
-    cout << "Normals size: " << depth_normals.size() << endl;
 
-
-
-//    int sensorFrame {0};
-//    while (sensor.processNextFrame()) {        { 0, 25, 50, 75, 100, 125, 150, 175, 200, 225, 255 },
-//
-//        std::cout << "reading data.." << std::endl;
-//        uint32_t depthWidth = sensor.getDepthImageWidth();
-//        uint32_t depthHeight = sensor.getDepthImageHeight();
-//
-//        cout << "size of depthMap: " << sizeof(depthMat) / sizeof(float) << endl;
-//        cout << "width: " << depthWidth << endl;
-//        cout << "height: " << depthHeight << endl;
-//        cout << "--------------------" << endl;
-//    }
-
-
-
-
-//    printCudaDeviceInfo(0);
     return 0;
 }
