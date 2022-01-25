4b29d333e420ba21746393060d546c18b56c9fd3
Nijat Ibrahimov <ibrahimovnijat@gmail.com>
2022-01-25 13:58:26 +0100

    new pose estimation added, needs revision

--- core/PoseEstimation.h
+++ core/PoseEstimation.h
@@ -7,6 +7,9 @@
 #endif //KINECTFUSION_POSEESTIMATION_H
 
 #include "Eigen.h"
+#include "declarations.h"
+
+using namespace Eigen;
 
 struct  Normal;
 struct Vertex;
@@ -21,113 +24,143 @@ private:
     const int pyramid_level = 3;
     const int iters[3] = {10,5,4};
 
-public:
-    PoseEstimation() {}
-    ~PoseEstimation() {}
+    unsigned int depthWidth, depthHeight;
+    Matrix4f transform_prev = Matrix4f::Zero(3,4);
 
-    Matrix4f convertPoseToMatrix(VectorXf& pose) {
+    int* vertex_validity;
+    unsigned int numValidPixels;
+    bool isValidCorrespondence = false;
+
+public: 
+
+    PoseEstimation() {
+
+    }
+    ~PoseEstimation() { std::cout << "PoseEstimation object destroyed!" << std::endl; }
+
+    void updateParams(unsigned int depthHeight, unsigned int depthWidth, int* vertex_validity, unsigned int numValidPixels) {
+        this->depthWidth = depthWidth;
+        this->depthHeight = depthHeight;
+        this->numValidPixels = numValidPixels;
+        for (unsigned int i =0; i < depthWidth * depthHeight; i++) {
+            this->vertex_validity[i] = vertex_validity[i];
+        }
+    }
+
+    static Matrix4f convertPoseToMatrix(VectorXf& pose) {
+
         Matrix4f transfer_increment = Matrix<float, 3,4>::Zero();
-
+        
         transfer_increment(0,0) = 1.0f;
-        transfer_increment(0,1) = pose(1);
-        transfer_increment(0,2) = -pose(2);
-        transfer_increment(0,3) = pose(4);
-        transfer_increment(1,0) = -pose(0);
-        transfer_increment(1,2) = pose(1);
-        transfer_increment(1,3) = pose(5);
-        transfer_increment(2,1) = pose(1);
-        //TODO finish the function
-    }
-    void convertMatrixToPose(Matrix4f& transform, VectorXf& pose) {}
+        transfer_increment(0,1) = pose(2);
+        transfer_increment(0,2) = -pose(1);
+        transfer_increment(0,3) = pose(3);
+        
+        transfer_increment(1,0) = -pose(2);
+        transfer_increment(1,1) = 1.0f;
+        transfer_increment(1,2) = pose(0);
+        transfer_increment(1,3) = pose(4);
 
-    static MatrixXf convertToGlobal(MatrixXf& local, Matrix4f& transform) {
-        MatrixXf global;
-//        global = transform * local;
-        for (unsigned int i = 0; i < local.rows(); i++) {
-            Vector4f current_local(local(i,0), local(i,1), local(i,2), 1);
-            Vector3f current_global = transform * current_local;
-            global.block(i,0,1,3) = current_global;
-        }
-        return global;  // can be normals or vertices etc.
+        transfer_increment(2,0) = pose(1);
+        transfer_increment(2,1) = -pose(0);
+        transfer_increment(2,2) = 1.0f;
+        transfer_increment(2,3) = pose(5);
+
+        return transfer_increment;
     }
 
-    static MatrixXf convertToLocal(MatrixXf global, Matrix4f transform) {
-        MatrixXf local;
-        MatrixXf transform_inv = transform.inverse();
-        for (unsigned int i = 0; i < global.rows(); i++) {
-            Vector3f current_global(global(i,0), global(i,1), global(i,2));
-            Vector4f current_local = transform_inv * current_global;
-            local.block(i,0,1,3) = current_local;
-            local(i, 3) = 1.0f;
-        }
-        return local;
+    static VectorXf convertMatrixToPose(Matrix4f& transform_increment) {
+        Vector4f poseVector = VectorXf::Zero(6,1);
+        poseVector[0] = transform_increment[1,2];
+        poseVector[1] = transform_increment[2,0];
+        poseVector[2] = transform_increment[0,1];
+        poseVector[3] = transform_increment[0,3];
+        poseVector[4] = transform_increment[1,3];
+        poseVector[5] = transform_increment[2,3];
+        return poseVector;
     }
 
-    static MatrixXf constructGmat(Vector3f& vertex) {
+   static Vector3f convertToGlobal(Vector4f& local, Matrix4f& transform) {
+       Vector3f glob = transform * local;
+//       Vector3f glob = Vector3f (glob_temp[0], glob_temp[1], glob_temp[2]);
+       return glob;
+ }
+
+    static Vector3f calcGlobalNormal(Vector3f& local, Matrix4f& transform) {
+
+        Matrix3f rot = transform.block(0,0,3,3);
+        Vector3f glob_norm = rot * local;
+        return glob_norm;
+    }
+   static Vector4f convertToLocal(Vector3f& global, Matrix4f transform) {
+
+       MatrixXf transform_inv = transform.inverse();
+       Vector3f local_temp = transform_inv * global;
+       Vector4f local = Vector4f(local_temp[0], local_temp[1], local_temp[2], 1.0f);
+       return local;
+   }
+
+    static MatrixXf constructGmat(Vertex& vertex) {
         MatrixXf G = Matrix<float, 3, 6>::Zero();
         G(0,0) = 0.0f;
-        G(0,1) = -vertex(2);
-        G(0,2) = vertex(1);
-        G(1,0) = vertex(2);
+        G(0,1) = -vertex.pos(2);
+        G(0,2) = vertex.pos(1);
+        G(1,0) = vertex.pos(2);
         G(1,1) = 0.0f;
-        G(1,2) = -vertex(0);
-        G(2,0) = -vertex(3);
-        G(2,1) = vertex(0);
+        G(1,2) = -vertex.pos(0);
+        G(2,0) = -vertex.pos(1);
+        G(2,1) = vertex.pos(0);
         G(2,2) = 0.0f;
         G.block(0,3, 3,3) = Matrix<float,3,3>::Identity();
         return G;
     }
 
-    void estimatePose(MatrixXf& vertices, MatrixXf& prevVertices, MatrixXf& normals, MatrixXf& prevNormals, Matrix4f& transform, Matrix4f& transform_prev) {
+    void estimatePose(Vertex* vertices, Normal* normals, Vertex& prevGlobVertices, Normal& prevGlobNormals, Matrix4f& transform) {
 
-        Eigen::Matrix3f rotation = transform.block(0,0,3,3);
-        Eigen::Vector3f translation = transform.block(0,3,3,1);
-        Eigen::Matrix3f rotation_prev = transform_prev.block(0,0,3,3);
-        Eigen::Vector3f translation_prev = transform_prev.block(0,3,3,1);
+        Eigen::Matrix3f rotation = transform.block(0, 0, 3, 3);
+        Eigen::Vector3f translation = transform.block(0, 3, 3, 1);
+        Eigen::Matrix3f rotation_prev = transform_prev.block(0, 0, 3, 3);
+        Eigen::Vector3f translation_prev = transform_prev.block(0, 3, 3, 1);
 
-        // do estimation from coarse to fine vertex sampling
         for (int level = 0; level < pyramid_level; level++) {
-            // different iterations for each levels
-            for(int iter= 0; iter < iters[level]; iter++) {
-                Eigen::MatrixXf A = Eigen::Matrix<float,6,6>::Zero();
-                Eigen::MatrixXf b = Eigen::Matrix<float,6,1>::Zero();
-                Eigen::MatrixXf G = Eigen::Matrix<float,3,6>::Zero();
+            for (int iter = 0; iter < iters[level]; iter++) {
 
-                bool isValidEstimation = false;
-                assert(normals.rows() ==  vertices.rows());  //check normal-vertex correspondence?
-                assert(prevNormals.rows() == prevVertices.rows()) ;
+                for (int i = 0; i < depthHeight; i++) {
+                    for (int j = 0; j < depthWidth; j++) {
+                        int curr_idx = i * depthWidth + j;
+                        if (vertex_validity[curr_idx] == 1) {
+                            MatrixXf A = Eigen::Matrix<float, 6, 6>::Zero();
+                            MatrixXf b = Eigen::Matrix<float, 6, 1>::Zero();
+                            MatrixXf G = Eigen::Matrix<float, 3, 6>::Zero();
 
-                Vector3f global_vertices = convertToGlobal(vertices, transform_prev);
-                Vector3f global_normals = convertToGlobal(normals, transform_prev);
-                Vector3f global_prevVertices = global_vertices;
-                Vector3f global_prevNormals = global_normals;
+                            Vertex Vg;
+                            Normal Ng;
+                            Vg.pos = convertToGlobal(vertices[i].pos,
+                                                     transform_prev);     //current global vertices and normals.
+                            Ng.val = calcGlobalNormal(normals[i].val, transform_prev);
 
-                for (unsigned int vertex = 0; vertex < vertices.rows(); vertex++) {
-                    //convert normals and vertices to global
-                    Vector3f glob_vertex = global_vertices.block(vertex,0, 1,3);
-                    Vector3f glob_normal = global_normals.block(vertex,0, 1,3);
-                    Vector3f glob_prevVertex = global_prevVertices.block(vertex, 0, 1, 3);
-                    Vector3f glob_prevNormal = global_prevNormals.block(vertex,0, 1,3);
+                            // TODO calculate vertex distances and normal angles
 
-                    //calculate A and b from G N and V matrices
-                    G = constructGmat(glob_vertex);
-                    A = (G.transpose() * glob_prevNormal).transpose();
-                    b = glob_prevNormal.transpose() * (glob_prevVertex - glob_vertex);
 
-                    // solve for x param vector using decomposition
-                    incrementPose = (A.transpose() * A).inverse() * A.transpose() * b;
+                            if (isValidCorrespondence) {
+                                G = constructGmat(Vg);
+                                A = (G.transpose() * prevGlobNormals.val).transpose();
+                                b = prevGlobNormals.val.transpose() * (prevGlobVertices.pos - Vg.pos);
 
-                    //convert pose param vector (x) to transform matrix
-                    transformIncrement = convertPoseToMatrix(incrementPose);
+                                // solve for pose vector
+                                incrementPose = (A.transpose() * A).inverse() * A.transpose() * b;
+                                // convert pose vector to transform matrix
+                                Matrix4f transform_increment_mat = convertPoseToMatrix(incrementPose);
+                                transform = transform_increment_mat * transform_prev.inverse(); // TODO check again
 
-                    // check validity of estimation
+                                transform_prev = transform;  // update transform for the next iter
+                            }
+                            else
+                            {
+                                continue;
+                            }
 
-                    if (isValidEstimation) {
-                        // update current transform matrix
-                        transform = transformIncrement * transform_prev;
-                        //update previous vertices and normals
-                        global_prevVertices = global_vertices;
-                        global_prevNormals = global_normals;
+                        }
                     }
                 }
             }
